b89bf574a128f81a90d6c8e6f1deac18de7709e8
giorgio <jorgeantonio82@gmail.com>
2025-01-28 15:07:13 +0100

    Initial project setup with Flask application and dependencies

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Hommie paish'
+
+@app.route('/greet')
+def greet():
+    return 'Hello, World!'
+
+@app.route('/greet/<name>')
+def greet_name(name):
+    return f'Hello, {name}! 😏'
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
